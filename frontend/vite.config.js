152cb21accf6266ffc11620bab09652c0371346d
RamNarayan0 <ramnarayan20070515@gmail.com>
2026-04-10 08:48:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -37,6 +37,7 @@ export default defineConfig(({ mode }) => {
       })
     ],
     server: {
+      host:'0.0.0.0',
       port: 5173,
       headers: {
         // Allow Google OAuth postMessage to work without being blocked by COOP
